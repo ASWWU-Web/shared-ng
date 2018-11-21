8af63601abcb88822461aeb8e547b415dba91a92
Stephen Ermshar <stephen.ermshar@gmail.com>
2018-11-21 14:29:11 -0800

    completed styling of mobile-nav-links, added link addresses

--- components/nav-bar/nav-bar.component.ts
+++ components/nav-bar/nav-bar.component.ts
@@ -16,17 +16,28 @@ export class NavBarComponent {
 
 	links: any = [
 		{ text: "ASWWU",       link: "/" }, 
-		{ text: "Mask",        link: "aswwu.com/mask" }, 
-		{ text: "Jobs",        link: "" },
-		{ text: "Upcomming",   link: "" },
+		{ text: "Mask",        link: "https://aswwu.com/mask" }, 
+		{ text: "Jobs",        link: "https://aswwu.com/jobs" },
+		{ text: "Upcomming",   link: "https://aswwu.com/#upcoming" },
 		{ text: "Departments", link: null, dropdownLinks: [
-			{ text: "Administration", 		link: "" },
-			{ text: "Atlas",          		link: "" },
-			{ text: "Diversity & Wellness",	link: ""}
+			{ text: "Administration", 		link: "https://aswwu.com/pages/administration" },
+			{ text: "Atlas",          		link: "https://aswwu.com/atlas" },
+			{ text: "Collegian",          	link: "https://aswwu.com/pages/collegian" },
+			{ text: "Global Service",       link: "https://aswwu.com/pages/globalservice" },
+			{ text: "Outdoors",          	link: "https://aswwu.com/pages/outdoors" },
+			{ text: "Photo",          		link: "https://aswwu.com/pages/photo" },
+			{ text: "Senate",          		link: "https://aswwu.com/pages/senate" },
+			{ text: "Social",          		link: "https://aswwu.com/pages/social" },
+			{ text: "Tread Shed",          	link: "https://aswwu.com/pages/treadshed" },
+			{ text: "Video",          		link: "https://aswwu.com/pages/video" },
+			{ text: "Diversity & Wellness",	link: "https://aswwu.com/pages/diversity-wellness"}
 		]},
 		{ text: "More", link: null, dropdownLinks: [
-			{ text: "D2L",   link: "" },
-			{ text: "Atlas", link: "" }
+			{ text: "D2L",   			link: "https://class.wallawalla.edu/" },
+			{ text: "MyWWU", 			link: "https://mywwu.wallawalla.edu/" },
+			{ text: "Office 365 Email", link: "https://office365.wallawalla.edu/" },
+			{ text: "WWU Homepage", 	link: "https://wallawalla.edu/" },
+			{ text: "Library Homepage", link: "https://www.wallawalla.edu/academics/libraries/" }
 		]},
 	];
 
