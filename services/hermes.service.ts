c5d7092abe49ee9a89c3c1415ee9dcf6b1b89935
Stephen Ermshar <stephen.ermshar@gmail.com>
2019-03-05 14:06:18 -0800

    add header image related code

--- services/hermes.service.ts
+++ services/hermes.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/internal/Subject';
+import { Observable } from 'rxjs/internal/Observable';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HermesService {
+  // http://jasonwatmore.com/post/2018/06/25/angular-6-communicating-between-components-with-observable-subject
+
+  private headerImageUriSubject = new Subject<string>();
 
   constructor() { }
+
+  sendHeaderImageUri(uri: string) {
+    this.headerImageUriSubject.next(uri);
+  }
+
 }
