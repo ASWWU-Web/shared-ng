5fab9037c2f8c783e728ca2fa2351f2bd71c1d2d
Stephen Ermshar <stephen.ermshar@gmail.com>
2019-03-05 14:31:16 -0800

    move app component out of shared ng

---
+++ components/shared-ng-container/shared-ng-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-ng-container',
+  templateUrl: './shared-ng-container.component.html',
+  styleUrls: ['./shared-ng-container.component.css']
+})
+export class SharedNgContainerComponent {
+}
