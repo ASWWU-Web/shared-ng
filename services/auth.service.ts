79f149cefd02aba8f479e31c81cac07e896c42b6
Stephen Ermshar <stephen.ermshar@gmail.com>
2019-03-12 13:42:37 -0700

    create auth service files

---
+++ services/auth.service.ts
@@ -0,0 +1,18 @@
+/*
+ * Created by: Stephen Ermshar and Sheldon Woodward
+ * Date: 2018-2019
+ *
+ * Note: Based on http://jasonwatmore.com/post/2018/10/29/angular-7-user-registration-and-login-example-tutorial#authentication-service-ts
+ */
+
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { HeaderButton, SubNavbarLink } from 'src/shared-ng/interfaces/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+}
--- services/services.ts
+++ services/services.ts
@@ -1,2 +1,3 @@
 export * from './request.service';
 export * from './hermes.service';
+export * from './auth.service';
