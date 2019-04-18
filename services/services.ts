c4e6eec29fe03c22e319cdd54c90153ee7bc822a
morini <nicholas.morin@wallawalla.edu>
2019-04-18 14:54:46 -0700

    added homepage.request.service

--- services/services.ts
+++ services/services.ts
@@ -1,4 +1,5 @@
 export * from './request.service';
 export * from './elections.request.service';
+export * from './homepage.request.service';
 export * from './hermes.service';
 export * from './auth.service';
