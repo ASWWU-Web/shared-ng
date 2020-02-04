3a7a311ac975277a842f436a4e19f6fac932482f
Nick <nick97eagles@gmail.com>
2020-02-04 14:35:46 -0800

    updated href to routerLink

--- components/nav-bar/nav-bar.component.ts
+++ components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 // tslint:disable:indent
+// tslint:disable:component-selector
+// tslint:disable:no-angle-bracket-type-assertion
 import { Component, Input } from '@angular/core';
 import { NgbPanelChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 import { SubNavbarLink } from '../../interfaces/interfaces';
@@ -37,37 +39,37 @@ export class NavBarComponent {
 	}
 
 	links: any = [
-		{ text: "Mask",        link: "https://aswwu.com/mask" },
-		{ text: "Jobs",        link: "https://aswwu.com/jobs" },
-		{ text: "Upcoming",   link: "https://aswwu.com/#upcoming" },
-		{ text: "Departments", link: null, dropdownLinks: [
-			{ text: "Administration", 		link: "https://aswwu.com/pages/administration" },
-			{ text: "Atlas",          		link: "https://aswwu.com/atlas" },
-			{ text: "Collegian",          	link: "https://www.aswwucollegian.com/" },
-			{ text: "Diversity & Wellness",	link: "https://aswwu.com/pages/diversity-wellness"},
-			{ text: "Global Service",       link: "https://aswwu.com/pages/globalservice" },
-			{ text: "Mountain Rents",       link: "https://aswwu.com/pages/mt-rents" },
-			{ text: "Outdoors",          	link: "https://aswwu.com/pages/outdoors" },
-      { text: "Photo",          		link: "https://aswwu.com/pages/photo" },
-      { text: "Productions",          		link: "https://aswwu.com/pages/aswwu-productions" },
-			{ text: "Senate",          		link: "https://aswwu.com/pages/senate" },
-			{ text: "Social",          		link: "https://aswwu.com/pages/social" },
-			{ text: "Tread Shed",          	link: "https://aswwu.com/pages/treadshed" },
-			{ text: "Video",          		link: "https://aswwu.com/pages/video" },
+		{ text: 'Mask',        link: '/mask' },
+		{ text: 'Jobs',        link: 'https://aswwu.com/jobs' },
+		{ text: 'Upcoming',   link: 'https://aswwu.com/#upcoming' },
+		{ text: 'Departments', link: null, dropdownLinks: [
+			{ text: 'Administration', 		link: 'https://aswwu.com/pages/administration' },
+			{ text: 'Atlas',          		link: 'https://aswwu.com/atlas' },
+			{ text: 'Collegian',          	link: 'https://www.aswwucollegian.com/' },
+			{ text: 'Diversity & Wellness',	link: 'https://aswwu.com/pages/diversity-wellness'},
+			{ text: 'Global Service',       link: 'https://aswwu.com/pages/globalservice' },
+			{ text: 'Mountain Rents',       link: 'https://aswwu.com/pages/mt-rents' },
+			{ text: 'Outdoors',          	link: 'https://aswwu.com/pages/outdoors' },
+      { text: 'Photo',          		link: 'https://aswwu.com/pages/photo' },
+      { text: 'Productions',          		link: 'https://aswwu.com/pages/aswwu-productions' },
+			{ text: 'Senate',          		link: 'https://aswwu.com/pages/senate' },
+			{ text: 'Social',          		link: 'https://aswwu.com/pages/social' },
+			{ text: 'Tread Shed',          	link: 'https://aswwu.com/pages/treadshed' },
+			{ text: 'Video',          		link: 'https://aswwu.com/pages/video' },
 		]},
-		{ text: "More", link: null, dropdownLinks: [
-			{ text: "D2L",   			link: "https://class.wallawalla.edu/" },
-			{ text: "MyWWU", 			link: "https://mywwu.wallawalla.edu/" },
-			{ text: "Office 365 Email", link: "https://office365.wallawalla.edu/" },
-			{ text: "WWU Homepage", 	link: "https://wallawalla.edu/" },
-			{ text: "Library Homepage", link: "https://www.wallawalla.edu/academics/libraries/" }
+		{ text: 'More', link: null, dropdownLinks: [
+			{ text: 'D2L',   			link: 'https://class.wallawalla.edu/' },
+			{ text: 'MyWWU', 			link: 'https://mywwu.wallawalla.edu/' },
+			{ text: 'Office 365 Email', link: 'https://office365.wallawalla.edu/' },
+			{ text: 'WWU Homepage', 	link: 'https://wallawalla.edu/' },
+			{ text: 'Library Homepage', link: 'https://www.wallawalla.edu/academics/libraries/' }
 		]},
 	];
 
 	public isCollapsed = false;
 
 	public beforeChange($event: NgbPanelChangeEvent) {
-		if ($event.panelId.startsWith("linkOnly")) {
+		if ($event.panelId.startsWith('linkOnly')) {
 		  $event.preventDefault();
 		}
 		}
