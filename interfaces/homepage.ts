3f984ab1304fb81d4bfff1f88cf36a02eefbf776
Michael Stacy <michael.stacy@wallawalla.edu>
2020-05-18 17:47:48 -0700

    added new notifications interface

--- interfaces/homepage.ts
+++ interfaces/homepage.ts
@@ -2,3 +2,14 @@ export interface Forum {
     message_body: string;
     recipient: string;
 }
+
+
+export interface Notification {
+  notification_text: string;
+  notification_links: string;
+  start_time: string;
+  end_time: string;
+  severity: number;
+  visibility: boolean;
+  // archived: boolean;
+}
