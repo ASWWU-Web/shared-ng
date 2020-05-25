e0529c661a6933982aea2bb1dfc75179062881d6
Nick <nick97eagles@gmail.com>
2020-05-25 15:01:56 -0700

    fixed tslint warning

--- services/request.service.ts
+++ services/request.service.ts
@@ -8,7 +8,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 
 import { environment } from '../../shared-ng/environments/environment';
 
