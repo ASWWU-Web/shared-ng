0f90aa24a5de160fbc9954b3ec8af66a0c9862b4
Matthew Ma <matthew.ma@wallawalla.edu>
2020-10-21 14:58:01 -0700

    Point SERVER_URL to aswwu.com

--- environments/environment.ts
+++ environments/environment.ts
@@ -6,6 +6,6 @@
 // You can run the server in production mode by running `ng serve -prod`.
 export const environment = {
   production: false,
-  SERVER_URL: "http://localhost:8888",
+  SERVER_URL: "https://aswwu.com/server",
   COOKIE_DOMAIN: ".localhost",
 };
