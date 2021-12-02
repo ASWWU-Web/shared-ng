6bdf9a6fef461229fd114d7ea3544e76b7db5305
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2021-12-01 16:24:42 -0800

    removing uncecessary file

--- components/progress-loader/progress-loader.component.ts
+++
@@ -1,7 +0,0 @@
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-
-// Reference: https://www.youtube.com/watch?v=o12iI18l1wI&ab_channel=ShaheerShukur
-//            https://material.angular.io/components/progress-spinner/overview
-
-// TODO add component.
-// TODO integrate into front-end
