a0e5be93bfb6063d9f90484cc35d31f10c178c93
Riley Smith <github@rileyy.dev>
2024-09-02 23:32:41 -0700

    simplify types for mask profile

--- interfaces/mask.ts
+++ interfaces/mask.ts
@@ -1,16 +1,3 @@
-export interface Profile {
-  username: string;
-  photo: string;
-  email: string;
-  full_name: string;
-  views: number | string;
-}
-
-export interface Names {
-  username: string;
-  full_name: string;
-}
-
 export interface ProfileFull {
   wwuid: string;
   username: string;
@@ -46,37 +33,10 @@ export interface ProfileFull {
   office: string;
   office_hours: string;
 }
+// For the name search endpoint
+export type Names = Pick<ProfileFull, 'username' | 'full_name'>;
 
-export interface ProfilePOST {
-  username: string;
-  full_name: string;
-  photo: string;
-  gender: string;
-  birthday: string;
-  email: string;
-  phone: string;
-  website: string;
-  majors: string;
-  minors: string;
-  graduate: string;
-  preprofessional: string;
-  class_standing: string;
-  high_school: string;
-  class_of: string;
-  relationship_status: string;
-  attached_to: string;
-  quote: string;
-  quote_author: string;
-  hobbies: string;
-  career_goals: string;
-  favorite_books: string;
-  favorite_movies: string;
-  favorite_music: string;
-  favorite_food: string;
-  pet_peeves: string;
-  personality: string;
-  privacy: string;
-  department: string;
-  office: string;
-  office_hours: string;
-}
+// Some of our endpoints only return a subset of the profile fields
+export type PartialProfile = Pick<ProfileFull, 'username' | 'full_name' | 'photo' | 'email'>;
+
+export type ProfileUpdate = Partial<Omit<ProfileFull, 'wwuid' | 'username' | 'views'>>;
